280b94cec071afd612e0e115501773c62920f729
Alexandre Pelletier <apelletier82@gmail.com>
2020-05-25 15:05:36 +0200

    nettoyage

--- Framework/Framework/Data/DBContexts/AbstractDBContext.cs
+++ Framework/Framework/Data/DBContexts/AbstractDBContext.cs
@@ -9,80 +9,79 @@ using System.Threading.Tasks;
 using Framework.Extensions;
 using System.Threading;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
-using Framework.Entities.Owned;
 
 namespace Framework.Data
 {
-    public abstract class AbstractDBContext : DbContext
-    {    
+    public abstract class AbstractDBContext : DbContext    
+    {
         private IIdentityUser _identityUser;
-        private ILogger<AbstractDBContext> _logger;       
-        
+        private ILogger<AbstractDBContext> _logger;
+
         public AbstractDBContext(DbContextOptions options, IIdentityUser identityUser, ILoggerFactory loggerFactory)
-            :base(options)
+            : base(options)
         {
             _identityUser = identityUser;
-            _logger = loggerFactory.CreateLogger<AbstractDBContext>();
+            _logger = loggerFactory.CreateLogger<AbstractDBContext>();                  
         }
 
         private void applyAuditToChangeTrackerEntries()
             => ChangeTracker.Entries()
                 .Where(e => ((e.Entity is IAuditable) &&
-                            (e.State == EntityState.Added || e.State == EntityState.Modified )))                
-                .ToList()               
-                .ForEach(item => 
-                    {                                                
-                        if (item.State == EntityState.Added)                        
-                            ((IAuditable)item.Entity).Creation.DoAudit(_identityUser.Username);                        
+                            (e.State == EntityState.Added || e.State == EntityState.Modified)))
+                .ToList()
+                .ForEach(item =>
+                    {
+                        if (item.State == EntityState.Added)
+                            ((IAuditable)item.Entity).Creation.DoAudit(_identityUser.Username);
                         // log last change everytime (on every change)                        
                         ((IAuditable)item.Entity).LastChange.DoAudit(_identityUser.Username);
-                    });                                                                              
+                    });
 
         private void applySoftDeleteToChangeTrackerEntries()
-        {  
+        {
             IList<EntityEntry> softDeletedEntites = new List<EntityEntry>();
-            EntityState newState = EntityState.Modified;
+            const EntityState CST_newState = EntityState.Modified;
 
             // if entity was already "soft" deleted : do nothing: just let delete it definitively
             ChangeTracker.Entries()
                 .Where(e => e.State == EntityState.Deleted && e.Entity is ISoftDeletable && ((ISoftDeletable)e.Entity).Deleted == false)
                 .ToList()
-                .ForEach(item => 
+                .ForEach(item =>
                 {
-                    item.State = newState; 
-                    ((ISoftDeletable)item.Entity).Delete(_identityUser.Username);                                         
-                    softDeletedEntites.Add(item);                                                                                                                         
-                });  
+                    item.State = CST_newState;
+                    ((ISoftDeletable)item.Entity).Delete(_identityUser.Username);
+                    softDeletedEntites.Add(item);
+                });
 
             // Synchronize owned "entities" EntityState with owner's EntityState (because when delete owned entities are set to deleted too)
             if (softDeletedEntites.Any())
                 ChangeTracker.Entries()
-                    .Where(e => e.State == EntityState.Deleted 
+                    .Where(e => e.State == EntityState.Deleted
                             && softDeletedEntites
                             .Any(any => any.Navigations
-                            .Any(navEntry => navEntry.CurrentValue == e.Entity)))                    
+                            .Any(navEntry => navEntry.CurrentValue == e.Entity)))
                     .ToList()
-                    .ForEach(item => item.State = newState );
+                    .ForEach(item => item.State = CST_newState);
         }
 
         public override int SaveChanges(bool acceptAllChangesOnSuccess)
         {
-            applySoftDeleteToChangeTrackerEntries();            
-            applyAuditToChangeTrackerEntries();                    
+            applySoftDeleteToChangeTrackerEntries();
+            applyAuditToChangeTrackerEntries();
             return base.SaveChanges(acceptAllChangesOnSuccess);
         }
 
         public override async Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default(CancellationToken))
-        {               
-            applySoftDeleteToChangeTrackerEntries();              
-            applyAuditToChangeTrackerEntries();                      
+        {
+            applySoftDeleteToChangeTrackerEntries();
+            applyAuditToChangeTrackerEntries();
             return await base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             _logger.LogDebug("OnModelCreating");
-            
+
             _logger.LogDebug("ApplyConfigurationFromInterfacedEntites");
             modelBuilder.ApplyConfigurationFromInterfacedEntites(this);
 
--- Framework/Framework/Exceptions/EntityNotFoundException.cs
+++ Framework/Framework/Exceptions/EntityNotFoundException.cs
@@ -2,22 +2,22 @@ using System;
 
 namespace Framework.Exceptions
 {
-    public class EntityNotFoundException: ApgcException
-    { 
-        public string EntityClassName {get; private set; }
+    public class EntityNotFoundException : ApgcException
+    {
+        public string EntityClassName { get; private set; }
 
         public EntityNotFoundException(string entityClassName, string message, Exception innerException)
             : base(message, innerException)
         {
             ExceptionType = ExceptionType.Business;
-            EntityClassName = entityClassName;            
+            EntityClassName = entityClassName;
         }
 
         public EntityNotFoundException(string entityClassName, string message)
             : this(entityClassName, message, null)
         { }
-        
-        public EntityNotFoundException(string entityClassName): this(entityClassName, "", null)
+
+        public EntityNotFoundException(string entityClassName) : this(entityClassName, "", null)
         { }
     }
 }
